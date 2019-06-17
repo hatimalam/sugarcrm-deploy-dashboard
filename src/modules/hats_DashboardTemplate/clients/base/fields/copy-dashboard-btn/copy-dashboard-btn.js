95dba3406ccbe32ad99d92534e1fa7c5d1c7e814
hatimalam <hatimalam@gmail.com>
2019-06-17 23:57:46 +0800

    #Move field and view level customization to core folder

---
+++ src/modules/hats_DashboardTemplate/clients/base/fields/copy-dashboard-btn/copy-dashboard-btn.js
@@ -0,0 +1,81 @@
+({
+    extendsFrom: 'RowactionField',
+    deployType: undefined,
+    initialize: function(options) {
+        this._super('initialize', [options]);
+        this.type = 'rowaction';
+    },
+
+    /** button click action */
+    rowActionSelect: function(evt) {
+        //check the deploy type from button metadata and open drawer accordingly
+        if(!_.isUndefined(this.def.deploy_type) && !_.isEmpty(this.def.deploy_type)) {
+            this.deployType = this.def.deploy_type;
+            console.log(this.deployType);
+            app.drawer.open({
+                    layout: 'multi-selection-list',
+                    context:
+                    {
+                        module: this.deployType,
+                        isMultiSelect: true,
+                    },
+                //on closing drawer, callback returns selection records array
+                }, _.bind(this.selectedUsersCallback, this)
+            );
+        } else {
+            app.alert.show('error_deploy_type', {
+                level: 'error',
+                messages: 'LBL_ERROR_INVALID_DEPLOY_TYPE',
+                autoClose: false
+            });
+        }
+        console.log('btn clicked..');
+    },
+
+    selectedUsersCallback: function(records) {
+    	console.log('callback from drawer');
+    	console.log(records);
+    	var selected_records = [];
+    	if(!_.isUndefined(records) && !_.isEmpty(records)) {
+    		_.each(records, function(record) {
+    			selected_records.push(record.id);
+    		});
+    		//call dashboard copy API to copy current template to all the selected IDs
+    		var self = this,
+                primary_user = this.model.get('assigned_user_id'),
+                template_id = this.model.get('id'),
+    			url = app.api.buildURL('hats_DashboardTemplate', 'copyUserDashboard', {}, {}),
+                value = {template_id: template_id, deploy_type:this.deployType, selected_records:selected_records};
+            app.api.call('create', url, value, {
+                success: function(data) {
+                    app.alert.dismiss('copy_dashboard_inprogress');
+                    if(data.success) {
+                        console.log(data);
+                        app.alert.show('dashboard_copy_success', {
+                            level: 'success',
+                            messages: app.lang.get('LBL_SUCCESSFULLY_COPIED_MESSAGE', self.module),
+                            autoClose: true,
+                        });
+                        //refresh the user subpanel
+                        self.model.getRelatedCollection('hats_dashboardtemplate_users').fetch({relate:true});
+                    } else {
+                        app.alert.show('dasboard_template_error', {
+                            level: 'error',
+                            messages: app.lang.get('LBL_DASHBOARD_TEMPLATE_ERROR', self.module),
+                            autoClose: false
+                        });
+                    }
+                },
+                error: function(err)
+                {
+                    if(err.status != 412) {
+                        app.alert.show('server-error',{level:'error',messages:'ERR_GENERIC_SERVER_ERROR'});
+                    }
+                    app.error.handleHttpError(err);
+                },
+            });
+    	} else {
+    		console.log('no action..');
+    	}
+    }
+})
---
+++ src/modules/hats_DashboardTemplate/clients/base/views/record/record.js
@@ -0,0 +1,96 @@
+({
+    extendsFrom: 'RecordView',
+    deployType: undefined,
+    initialize: function(options)
+    {
+        this._super('initialize', [options]);
+        this.context.on("button:deploy_template_user_btn:click", this.saveDashboard, this);
+        this.context.on("button:deploy_template_team_btn:click", this._deployData, this);
+        this.context.on("button:deploy_template_role_btn:click", this._deployData, this);
+    },
+
+    _deployData: function(evt) {
+        console.log(evt);
+        console.log(this.context);
+    },
+
+    saveDashboard: function()
+    {
+        console.log("Deploy dashboard button clicked");
+        //open mutliple-selection list drawer
+        app.drawer.open({
+                layout: 'multi-selection-list',
+                context:
+                {
+                    module: 'Users',
+                    isMultiSelect: true,
+                },
+            //on closing drawer, callback returns selection records array
+            }, _.bind(this.selectedUsersCallback, this)
+        );
+    },
+
+    selectedUsersCallback: function(models)
+    {
+        console.log("Selected users callback called");
+        // console.log(model);
+        //iterate over each model
+        if(!_.isEmpty(models))
+        {
+            app.alert.show('select_user_confirmation', {
+                title: app.lang.get('LBL_USER_OVERWRITE_WARNING', this.module),
+                level: 'confirmation',
+                messages: app.lang.get('LBL_OVERWRITE_CONFIRMATION_MESSAGE', this.module),
+                onConfirm: _.bind(function() {
+                    app.alert.show('copy_dashboard_inprogress', {
+                        level: 'process',
+                        title: app.lang.get('LBL_COPYING_DASHBOARDS', this.module),
+                    });
+                    var user_ids = [];
+                    _.each(models, function(model) {
+                        user_ids.push(model.id);
+                    });
+                    var self = this,
+                        primary_user = this.model.get('assigned_user_id'),
+                        template_id = this.model.get('id'),
+                        //pass these ids to copyUserDashboard API
+                        url = app.api.buildURL('hats_DashboardTemplate', 'copyUserDashboard', {}, {}),
+                        value = {template_id: template_id, primary_user:primary_user, secondary_users:user_ids};
+                    //call custom API to copy dashboard data
+                    app.api.call('create', url, value, {
+                        success: function(data)
+                        {
+                            if(data.success)
+                            {
+                                app.alert.dismiss('copy_dashboard_inprogress');
+                                console.log(data);
+                                app.alert.show('dashboard_copy_success', {
+                                    level: 'success',
+                                    messages: app.lang.get('LBL_SUCCESSFULLY_COPIED_MESSAGE', self.module),
+                                    autoClose: true,
+                                });
+                                //refresh the user subpanel
+                                self.model.getRelatedCollection('hats_dashboardtemplate_users').fetch({relate:true});
+                            }
+                        },
+                        error: function(err)
+                        {
+                            if(err.status != 412) {
+                                app.alert.show('server-error',{level:'error',messages:'ERR_GENERIC_SERVER_ERROR'});
+                            }
+                            app.error.handleHttpError(err);
+                        },
+                    });
+                }, this),
+            });
+
+        } else {
+            app.alert.show('user_cancelled_operation', {
+                level: 'error',
+                title: app.lang.get('LBL_COPY_OPERATION_CANCELLED', this.module),
+                messages: app.lang.get('LBL_COPY_OPERATION_CANCELLED_MSG', this.module),
+                autoClose: true,
+            });
+        }
+    },
+})
